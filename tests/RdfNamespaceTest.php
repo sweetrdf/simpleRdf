1974025edcc1a812eb26f845acae075093819fb9
Mateusz Żółtak <zozlak@zozlak.org>
2021-03-16 17:46:23 +0100

    RdfNamespace tests moved to rdfInterface library

--- tests/RdfNamespaceTest.php
+++ tests/RdfNamespaceTest.php
@@ -26,93 +26,12 @@
 
 namespace simpleRdf;
 
-use OutOfBoundsException;
-use BadMethodCallException;
-
 /**
  * Description of TestRdfNamespace
  *
  * @author zozlak
  */
-class RdfNamespaceTest extends \PHPUnit\Framework\TestCase {
+class RdfNamespaceTest extends \rdfInterface\tests\RdfNamespaceTest {
 
     use TestTrait;
-    use \rdfInterface\tests\TestBaseTrait;
-
-    public function testGetAddRemove(): void {
-        $nmsp = self::getRdfNamespace();
-
-        $sn = $nmsp->add('http://foo');
-        $this->assertArrayHasKey($sn, $nmsp->getAll());
-        $this->assertEquals('http://foo', $nmsp->getAll()[$sn]);
-        $this->assertEquals('http://foo', $nmsp->get($sn));
-
-        $sn = $nmsp->add('http://bar', 'baz');
-        $this->assertEquals('baz', $sn);
-        $this->assertArrayHasKey('baz', $nmsp->getAll());
-        $this->assertEquals('http://bar', $nmsp->getAll()['baz']);
-        $this->assertEquals('http://bar', $nmsp->get('baz'));
-
-        $sn = $nmsp->add('http://baz', 'baz');
-        $this->assertEquals('baz', $sn);
-        $this->assertArrayHasKey('baz', $nmsp->getAll());
-        $this->assertEquals('http://baz', $nmsp->getAll()['baz']);
-        $this->assertEquals('http://baz', $nmsp->get('baz'));
-
-        $nmsp->remove('baz');
-        $this->assertArrayNotHasKey('baz', $nmsp->getAll());
-        try {
-            $nmsp->get('baz');
-            $this->assertTrue(false);
-        } catch (OutOfBoundsException $ex) {
-            
-        }
-    }
-
-    public function testShorten(): void {
-        $nmsp = self::getRdfNamespace();
-        $nmsp->add('http://foo/', 'n1');
-
-        $this->assertEquals('n1:bar', $nmsp->shorten(self::$df::namedNode('http://foo/bar'), false));
-
-        $sn1 = $nmsp->shorten(self::$df::namedNode('http://bar#baz'), true);
-        $p   = strpos($sn1, ':');
-        $this->assertStringEndsWith(':baz', $sn1);
-        $sn2 = $nmsp->shorten(self::$df::namedNode('http://bar#foo'), false);
-        $this->assertStringEndsWith(':foo', $sn2);
-        $this->assertEquals(substr($sn1, 0, $p), substr($sn2, 0, $p));
-        $sn3 = $nmsp->shorten(self::$df::namedNode('http://bar#foo/'), false);
-        $this->assertStringEndsWith(':foo/', $sn3);
-        $this->assertEquals(substr($sn1, 0, $p), substr($sn3, 0, $p));
-
-        try {
-            $nmsp->shorten(self::$df::namedNode('http://foobar/baz'), false);
-            $this->assertTrue(false);
-        } catch (OutOfBoundsException $ex) {
-            
-        }
-    }
-
-    public function testExpand(): void {
-        $nmsp = self::getRdfNamespace();
-        $nmsp->add('http://foo/', 'n1');
-        $n2   = $nmsp->add('http://bar#');
-
-        $this->assertEquals('http://foo/baz', $nmsp->expand('n1:baz')->getValue());
-        $this->assertEquals('http://bar#baz', $nmsp->expand($n2 . ':baz')->getValue());
-
-        try {
-            $nmsp->expand('foobar:baz');
-            $this->assertTrue(false);
-        } catch (OutOfBoundsException $ex) {
-            
-        }
-
-        try {
-            $nmsp->expand('foobar');
-            $this->assertTrue(false);
-        } catch (BadMethodCallException $ex) {
-            
-        }
-    }
 }
